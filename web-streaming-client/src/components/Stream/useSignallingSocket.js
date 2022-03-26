86fe5e189aacc35d4ab1bdf2bab333dbee26d00a
Simon Stucki <simon_stucki@hotmail.com>
2022-03-26 15:27:46 +0100

    Refactor signalling socket handler

--- web-streaming-client/src/components/Stream/useSignallingSocket.js
+++ web-streaming-client/src/components/Stream/useSignallingSocket.js
@@ -2,19 +2,34 @@ import { useEffect } from "react";
 import { useLogger } from "../../core/logger";
 import { SIGNALLING_SERVER_URL } from "./Stream";
 
+async function applyOffer(peerConnection, socket, remoteDescription) {
+  await peerConnection.setRemoteDescription(remoteDescription);
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+  socket.send(
+    new Blob(
+      [
+        JSON.stringify({
+          type: "SessionDescription",
+          payload: answer,
+        }),
+      ],
+      { type: "application/json" }
+    )
+  );
+}
+
 function useSignallingSocket(socketRef, peerConnectionRef, send) {
   const logger = useLogger();
   useEffect(() => {
-    // Create WebSocket connection.
     const socket = new WebSocket(SIGNALLING_SERVER_URL);
     socketRef.current = socket;
-    // Connection opened
+
     socket.addEventListener("open", () => {
       logger.log("opened connection to signalling server");
       send("ESTABLISHED");
     });
 
-    // Listen for messages
     socket.addEventListener("message", function (event) {
       (async () => {
         const text = await event.data.text();
@@ -22,24 +37,7 @@ function useSignallingSocket(socketRef, peerConnectionRef, send) {
 
         if (json.payload.type === "offer") {
           logger.log("apply remote session description");
-          await peerConnectionRef.current.setRemoteDescription(json.payload);
-          try {
-            const answer = await peerConnectionRef.current.createAnswer();
-            await peerConnectionRef.current.setLocalDescription(answer);
-            socket.send(
-              new Blob(
-                [
-                  JSON.stringify({
-                    type: "SessionDescription",
-                    payload: answer,
-                  }),
-                ],
-                { type: "application/json" }
-              )
-            );
-          } catch (e) {
-            logger.error("create answer error", e);
-          }
+          await applyOffer(peerConnectionRef.current, socket, json.payload);
         } else {
           // ice candidate
           peerConnectionRef.current.addIceCandidate(json.payload);
