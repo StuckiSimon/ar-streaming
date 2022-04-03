f6de4b865eb06393207868a6d039644d7b481747
Simon Stucki <simon_stucki@hotmail.com>
2022-04-03 15:21:52 +0200

    Improve PointCloudView
    Use FlyControls.
    Add lookAt to camera.
    Tune z distance.

--- web-streaming-client/src/components/PointCloudView.js
+++ web-streaming-client/src/components/PointCloudView.js
@@ -1,11 +1,13 @@
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { FlyControls } from "@react-three/drei";
 import styles from "./PointCloudView.module.scss";
 
-function PointCloudView({ depthData }) {
+function PointCloud({ depthData }) {
   const [mesh, setMesh] = useState(null);
+  const cameraRef = useRef(null);
+  const counter = useRef(0);
 
   useEffect(() => {
     if (depthData) {
@@ -21,7 +23,7 @@ function PointCloudView({ depthData }) {
         const downScale = 5;
         const x = (index % 256) / downScale;
         const y = Math.floor(index / 256) / downScale;
-        const z = depth * 10;
+        const z = depth * 100;
 
         positions.push(x, y, z);
 
@@ -47,15 +49,52 @@ function PointCloudView({ depthData }) {
       });
 
       const points = new THREE.Points(geometry, material);
+      counter.current++;
       setMesh(points);
     }
   }, [depthData]);
+
+  useThree(({ camera }) => {
+    cameraRef.current = camera;
+  });
+
+  useEffect(() => {
+    if (!mesh || counter.current > 1) {
+      return;
+    }
+    // look at center of plane
+    const middle = new THREE.Vector3();
+    const pointsGeometry = mesh.geometry;
+
+    pointsGeometry.computeBoundingBox();
+
+    middle.x =
+      (pointsGeometry.boundingBox.max.x + pointsGeometry.boundingBox.min.x) / 2;
+    middle.y =
+      (pointsGeometry.boundingBox.max.y + pointsGeometry.boundingBox.min.y) / 2;
+    middle.z =
+      (pointsGeometry.boundingBox.max.z + pointsGeometry.boundingBox.min.z) / 2;
+
+    cameraRef.current?.lookAt(middle);
+  }, [mesh]);
+
+  return (
+    <Suspense fallback={null}>
+      {mesh ? <primitive object={mesh} /> : null}
+      <FlyControls
+        movementSpeed={4.0}
+        rollSpeed={0.5}
+        autoForward={false}
+        dragToLook={true}
+      />
+    </Suspense>
+  );
+}
+
+function PointCloudView({ depthData }) {
   return (
     <Canvas className={styles.root}>
-      <Suspense fallback={null}>
-        {mesh ? <primitive object={mesh} /> : null}
-        <OrbitControls />
-      </Suspense>
+      <PointCloud depthData={depthData} />
     </Canvas>
   );
 }
